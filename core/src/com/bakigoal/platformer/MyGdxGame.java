5577632d3c0b665b1071aad02d51a8ae8db2f151
Ilmir <ilmirbakirov@gmail.com>
2017-04-03 17:55:05 +0500

    added assets & TextureRegions rendering concept

--- core/src/com/bakigoal/platformer/MyGdxGame.java
+++ core/src/com/bakigoal/platformer/MyGdxGame.java
@@ -1,12 +1,31 @@
 package com.bakigoal.platformer;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 import com.bakigoal.platformer.samples.SpriteBatchScreen;
+import com.bakigoal.platformer.samples.TextureAtlasScreen;
+import com.bakigoal.platformer.screens.MenuScreen;
 
 public class MyGdxGame extends Game {
 
   @Override
   public void create() {
-    setScreen(new SpriteBatchScreen());
+//    setScreen(new MenuScreen());
+    setScreen(SampleScreens.TEXTURE_ATLAS);
+  }
+
+  private void setScreen(SampleScreens screen) {
+    setScreen(screen.screen);
+  }
+
+  private enum SampleScreens {
+    SPRITE_BATCH(new SpriteBatchScreen()),
+    TEXTURE_ATLAS(new TextureAtlasScreen());
+
+    private final Screen screen;
+
+    SampleScreens(Screen screen) {
+      this.screen = screen;
+    }
   }
 }
