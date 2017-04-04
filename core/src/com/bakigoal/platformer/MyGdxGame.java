92604d8d893eb74ad08ba5169f3c556e5b41a8c5
Ilmir <ilmirbakirov@gmail.com>
2017-04-04 13:25:22 +0500

    ShapeRenderer sample screen

--- core/src/com/bakigoal/platformer/MyGdxGame.java
+++ core/src/com/bakigoal/platformer/MyGdxGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.bakigoal.platformer.samples.AnimatedSpriteScreen;
 import com.bakigoal.platformer.samples.OrthographicCameraScreen;
+import com.bakigoal.platformer.samples.ShapeRendererScreen;
 import com.bakigoal.platformer.samples.SpriteBatchScreen;
 import com.bakigoal.platformer.samples.SpriteScreen;
 import com.bakigoal.platformer.samples.TextureAtlasScreen;
@@ -13,7 +14,7 @@ public class MyGdxGame extends Game {
   @Override
   public void create() {
 //    setScreen(new MenuScreen());
-    setScreen(SampleScreens.ORTHOGRAPHIC_CAMERA);
+    setScreen(SampleScreens.SHAPE_RENDERER);
   }
 
   private void setScreen(SampleScreens screen) {
@@ -25,7 +26,8 @@ public class MyGdxGame extends Game {
     TEXTURE_ATLAS(new TextureAtlasScreen()),
     SPRITES(new SpriteScreen()),
     SPRITES_ANIMATION(new AnimatedSpriteScreen()),
-    ORTHOGRAPHIC_CAMERA(new OrthographicCameraScreen());
+    ORTHOGRAPHIC_CAMERA(new OrthographicCameraScreen()),
+    SHAPE_RENDERER(new ShapeRendererScreen());
 
     private final Screen screen;
 
