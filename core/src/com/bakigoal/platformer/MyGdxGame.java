ad079bd38a6c7465a81180fcc8948583dc3587cf
Ilmir <ilmirbakirov@gmail.com>
2017-04-04 12:05:00 +0500

    Orthographic camera zoom, move

--- core/src/com/bakigoal/platformer/MyGdxGame.java
+++ core/src/com/bakigoal/platformer/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.bakigoal.platformer;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.bakigoal.platformer.samples.AnimatedSpriteScreen;
+import com.bakigoal.platformer.samples.OrthographicCameraScreen;
 import com.bakigoal.platformer.samples.SpriteBatchScreen;
 import com.bakigoal.platformer.samples.SpriteScreen;
 import com.bakigoal.platformer.samples.TextureAtlasScreen;
@@ -12,7 +13,7 @@ public class MyGdxGame extends Game {
   @Override
   public void create() {
 //    setScreen(new MenuScreen());
-    setScreen(SampleScreens.SPRITES_ANIMATION);
+    setScreen(SampleScreens.ORTHOGRAPHIC_CAMERA);
   }
 
   private void setScreen(SampleScreens screen) {
@@ -23,7 +24,8 @@ public class MyGdxGame extends Game {
     SPRITE_BATCH(new SpriteBatchScreen()),
     TEXTURE_ATLAS(new TextureAtlasScreen()),
     SPRITES(new SpriteScreen()),
-    SPRITES_ANIMATION(new AnimatedSpriteScreen());
+    SPRITES_ANIMATION(new AnimatedSpriteScreen()),
+    ORTHOGRAPHIC_CAMERA(new OrthographicCameraScreen());
 
     private final Screen screen;
 
